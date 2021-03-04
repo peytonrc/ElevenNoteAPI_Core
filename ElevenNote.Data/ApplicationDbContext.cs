81f276f5941fb3d04d5091ef9abc0a6899a71be0
peytonrc <peytnrose@gmail.com>
2021-03-04 14:35:45 -0500

    app db

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
+using ElevenNote.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-       : base(options)
-     {
-     }
-    }
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+	: base(options)
+		{
+		}
+
+         public DbSet<UserEntity> Users { get; set; }
+        
+	}
 }
